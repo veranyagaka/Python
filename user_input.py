897a3d281ec874913872173ff816ff2e1a5a1e31
veranyagaka <nyagakaver@gmail.com>
2024-02-28 20:27:57 +0300

    Second week

--- user_input.py
+++ user_input.py
@@ -2,3 +2,10 @@ name=input("Enter your name:")
 age=input("Enter your age:")
 location=input("Enter your location:")
 print("Hello " + name + ", you are " + age + " years old and live in " + location)
+
+x ={}
+x[2]=10
+x[1]=[20,30,40]
+print(x[1][2])
+# this a comment
+#second week
