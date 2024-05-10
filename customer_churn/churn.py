b2cd2c1205fa9e2130370b6b54d0b68be100b947
veranyagaka <nyagakaver@gmail.com>
2024-05-10 23:00:08 +0300

    customer churn

---
+++ customer_churn/churn.py
@@ -0,0 +1,39 @@
+#libraries
+import pandas as pd
+import numpy as np
+import missingno as msno
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+from sklearn import metrics
+from sklearn.metrics import roc_curve
+from sklearn.metrics import recall_score, confusion_matrix, precision_score, f1_score, accuracy_score, classification_report
+
+#loading data
+df = pd.read_csv('/home/vera/Desktop/dev/python/customer_churn/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+print(df.head())
+print(df.shape)
+print(df.info())
+print(df.columns.values)
